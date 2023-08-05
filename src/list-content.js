d7d3978cca457398761a709bb1aacc3beff0ca7b
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-08-05 15:01:14 +0530

    Add edit list feature
    Update list-content.js
    
    Update function updateListContainer
    - Create following elements
      - editListItemButton
      - editListModal
      - editListForm
      - editListInput
      - editListSubmitButton
    - Add class to above elements
    - Add attribute method="dialog" to element editListForm
    
    Create function editListItem
    - This function will edit list item name

--- src/list-content.js
+++ src/list-content.js
@@ -14,19 +14,39 @@ export function ListContent(todoList) {
     function updateListContainer(item) {
         const listItem = document.createElement("p");
         const deleteListItemButton = document.createElement("button");
+        const editListItemButton = document.createElement("button");
+        const editListModal = document.createElement("dialog");
+        const editListForm = document.createElement("form");
+        const editListInput = document.createElement("input");
+        const editListSubmitButton = document.createElement("button");
         const listItemContainer = document.createElement("div");
 
         listItem.textContent = item;
         deleteListItemButton.textContent = "\u2A09";
+        editListItemButton.textContent = "\u270F";
+        editListSubmitButton.textContent = "Submit";
 
         listItem.classList.add("list-item");
         deleteListItemButton.classList.add("delete-list-item-button");
+        editListItemButton.classList.add("edit-list-item-container");
+        editListModal.classList.add("edit-list-modal");
+        editListForm.classList.add("edit-list-form");
+        editListInput.classList.add("edit-list-input");
+        editListSubmitButton.classList.add("edit-list-submit-button");
         listItemContainer.classList.add("list-item-container");
 
+        editListForm.setAttribute("method", "dialog");
+
         deleteListItem(deleteListItemButton);
+        editListItem(editListItemButton, editListModal, editListInput, editListSubmitButton);
 
         listItemContainer.appendChild(listItem);
         listItemContainer.appendChild(deleteListItemButton);
+        listItemContainer.appendChild(editListItemButton);
+        editListForm.appendChild(editListInput);
+        editListForm.appendChild(editListSubmitButton);
+        editListModal.appendChild(editListForm);
+        listItemContainer.appendChild(editListModal);
         listContainer.appendChild(listItemContainer);
     }
 
@@ -50,7 +70,20 @@ export function ListContent(todoList) {
             todoList.deleteList(this.previousElementSibling.textContent);
             this.parentElement.remove();
             console.log(todoList);
-        })
+        });
+    }
+
+    function editListItem(editButton, editListModal, editListInput, editListSubmitButton) {
+        editButton.addEventListener("click", function() {
+            editListModal.show();
+            editListSubmitButton.addEventListener("click", function() {
+                if (editListInput.value) {
+                    todoList.renameList(editButton.parentElement.children[0].textContent, editListInput.value);
+                    editButton.parentElement.children[0].textContent = editListInput.value;
+                }
+                console.log(todoList);
+            });
+        });
     }
 
     renderListContainer(todoList.list)
