8a996485886162b8dee5643c3254d8d1de2cb8e0
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-08-05 17:26:33 +0530

    Refactor the code
    Update index.html
    - Change following class names
      - .add-list-button to .add-list-content
      - .list-name to add-list-input
      - .submit-list-button to .add-list-submit-button
    Add class .add-list-form to form
    Add class .add-list-button to button
    
    Update list-content.js
    - Change variable names
    - Update function editListItem

--- src/list-content.js
+++ src/list-content.js
@@ -2,8 +2,8 @@ export function ListContent(todoList) {
     const listContainer = document.querySelector(".list-container");
     const addListButton = document.querySelector(".add-list-button");
     const addListModal = document.querySelector(".add-list-modal");
-    const submitListButton = document.querySelector(".submit-list-button");
-    const listName = document.querySelector(".list-name");
+    const addListSubmitButton = document.querySelector(".add-list-submit-button");
+    const addListInput = document.querySelector(".add-list-input");
 
     function renderListContainer(list) {
         list.forEach(item => {
@@ -15,38 +15,46 @@ export function ListContent(todoList) {
         const listItem = document.createElement("p");
         const deleteListItemButton = document.createElement("button");
         const editListItemButton = document.createElement("button");
-        const editListModal = document.createElement("dialog");
-        const editListForm = document.createElement("form");
-        const editListInput = document.createElement("input");
-        const editListSubmitButton = document.createElement("button");
+        const editListItemModal = document.createElement("dialog");
+        const editListItemForm = document.createElement("form");
+        const editListItemInput = document.createElement("input");
+        const editListItemSubmitButton = document.createElement("button");
         const listItemContainer = document.createElement("div");
 
         listItem.textContent = item;
         deleteListItemButton.textContent = "\u2A09";
         editListItemButton.textContent = "\u270F";
-        editListSubmitButton.textContent = "Submit";
+        editListItemSubmitButton.textContent = "Submit";
 
         listItem.classList.add("list-item");
         deleteListItemButton.classList.add("delete-list-item-button");
         editListItemButton.classList.add("edit-list-item-container");
-        editListModal.classList.add("edit-list-modal");
-        editListForm.classList.add("edit-list-form");
-        editListInput.classList.add("edit-list-input");
-        editListSubmitButton.classList.add("edit-list-submit-button");
+        editListItemModal.classList.add("edit-list-modal");
+        editListItemForm.classList.add("edit-list-form");
+        editListItemInput.classList.add("edit-list-input");
+        editListItemSubmitButton.classList.add("edit-list-submit-button");
         listItemContainer.classList.add("list-item-container");
 
-        editListForm.setAttribute("method", "dialog");
+        editListItemForm.setAttribute("method", "dialog");
 
         deleteListItem(deleteListItemButton);
-        editListItem(editListItemButton, editListModal, editListInput, editListSubmitButton);
+        editListItem(
+            {
+                listItem,
+                editListItemButton, 
+                editListItemModal, 
+                editListItemInput, 
+                editListItemSubmitButton
+            }
+        );
 
         listItemContainer.appendChild(listItem);
         listItemContainer.appendChild(deleteListItemButton);
         listItemContainer.appendChild(editListItemButton);
-        editListForm.appendChild(editListInput);
-        editListForm.appendChild(editListSubmitButton);
-        editListModal.appendChild(editListForm);
-        listItemContainer.appendChild(editListModal);
+        editListItemForm.appendChild(editListItemInput);
+        editListItemForm.appendChild(editListItemSubmitButton);
+        editListItemModal.appendChild(editListItemForm);
+        listItemContainer.appendChild(editListItemModal);
         listContainer.appendChild(listItemContainer);
     }
 
@@ -56,30 +64,30 @@ export function ListContent(todoList) {
             // addListModal.showModal()
         });
         
-        submitListButton.addEventListener("click", function() {
-            if (listName.value) {
-                item.createList(listName.value);
-                updateListContainer(listName.value);
+        addListSubmitButton.addEventListener("click", function() {
+            if (addListInput.value) {
+                item.createList(addListInput.value);
+                updateListContainer(addListInput.value);
             }
             console.log(item);
         });
     }
 
-    function deleteListItem(button) {
-        button.addEventListener("click", function() {
+    function deleteListItem(deleteListItemButton) {
+        deleteListItemButton.addEventListener("click", function() {
             todoList.deleteList(this.previousElementSibling.textContent);
             this.parentElement.remove();
             console.log(todoList);
         });
     }
 
-    function editListItem(editButton, editListModal, editListInput, editListSubmitButton) {
-        editButton.addEventListener("click", function() {
-            editListModal.show();
-            editListSubmitButton.addEventListener("click", function() {
-                if (editListInput.value) {
-                    todoList.renameList(editButton.parentElement.children[0].textContent, editListInput.value);
-                    editButton.parentElement.children[0].textContent = editListInput.value;
+    function editListItem(item) {
+        item.editListItemButton.addEventListener("click", function() {
+            item.editListItemModal.show();
+            item.editListItemSubmitButton.addEventListener("click", function() {
+                if (item.editListItemInput.value) {
+                    todoList.renameList(item.listItem.textContent, item.editListItemInput.value);
+                    item.listItem.textContent = item.editListItemInput.value;
                 }
                 console.log(todoList);
             });
