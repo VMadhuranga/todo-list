e76ed0ab6f2aac1869aaa03fd145722221cf40ea
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-08-06 18:40:55 +0530

    Create task content
    Create task-content.js
    
    Update task-content.js
    - Create function TaskContent
      - This function will take todoList, listTask its argument
    - Add click event listener to listTask
      - This will update taskContainer with relevant list name and
        it's tasks
    - Create function renderTaskContainer
      - This function will get all the tasks in list return that task
    
    Update list-content.js
    - Import function TaskContent

--- src/task-content.js
+++ src/task-content.js
@@ -1,6 +1,37 @@
 export function TaskContent(todoList, listItem) {
+    const taskContainer = document.querySelector(".task-container");
+    const taskListTitle = document.querySelector(".task-list-title");
+
     listItem.addEventListener("click", function() {
-        console.log(this.textContent);
-        console.log(todoList.getList(this.textContent));
+        taskListTitle.textContent = this.textContent;
+        if (taskContainer.children.length === 0) {
+            taskContainer.appendChild(
+            renderTaskContainer(this.textContent, todoList.getList(this.textContent)));
+        } else {
+            taskContainer.replaceChild(
+                renderTaskContainer(this.textContent, todoList.getList(this.textContent)),
+                taskContainer.children[0]);
+        }
     });
+
+    function renderTaskContainer(listItem, task) {
+        const listTask = document.createElement("div");
+
+        for (let i = 0; i < task.length; i++) {
+            const taskContent = document.createElement("div");
+            const taskTitle = document.createElement("h3");
+            const taskDescription = document.createElement("p");
+            const taskDate = document.createElement("p");
+
+            taskTitle.textContent = todoList.getListTask(listItem, task[i].title).title;
+            taskDescription.textContent = todoList.getListTask(listItem, task[i].title).description;
+            taskDate.textContent = todoList.getListTask(listItem, task[i].title).date;
+
+            taskContent.appendChild(taskTitle);
+            taskContent.appendChild(taskDescription);
+            taskContent.appendChild(taskDate);
+            listTask.appendChild(taskContent);
+        }
+        return listTask;
+    }
 }
